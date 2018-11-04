39bceb3b162faace2ccafd181d757c80323d2af7
Rupert <rupert.dudeney@gmail.com>
2018-11-04 10:46:10 -0500

    Updated index file and GameController to functioning file

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -6,14 +6,153 @@ use Illuminate\Http\Request;
 
 class GameController extends Controller
 {
-    public function index()
+
+
+    private $TOTAL = 6; # Total constant of all door values
+
+    public function index(Request $request)
     {
-        return view('index');
+        return view('game.index')->with([
+            'type' => $request->session()->get('type', null),
+            'repetitions' => $request->session()->get('repetitions', null),
+            'guess' => $request->session()->get('guess', null),
+            'num_correct' => $request->session()->get('num_correct', null),
+            'percentage' => $request->session()->get('percentage', null),
+            'response' => $request->session()->get('response', null),
+        ]);
     }
 
-    public function processInput()
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function processInput(Request $request)
     {
-        return 'Process the input and redirect user back to the form with the results';
+
+        # Parameter confirms whether to change or stay
+        $type = $request->input('type');
+        $repetitions = $request->input('repetitions');
+        $guess = $request->input('guess');
+
+        if ($type && $repetitions && $guess)
+        {
+            $num_correct = $this->process($type, $repetitions);
+            $percentage = ($num_correct / $repetitions) * 100;
+            $response = $this->respond($guess, $num_correct);
+        }
+
+        return redirect('/')->with([
+            'type' => $type,
+            'repetitions' => $repetitions,
+            'guess' => $guess,
+            'num_correct' => $num_correct,
+            'percentage' => $percentage,
+            'response' => $response,
+        ]);
+    }
+
+    /**
+    * @param $guess
+    * @param $num_correct
+    * @return string
+
+    * Method compares the user guess to the results and assigns response to be passed to
+    *user
+    */
+
+    private function respond($guess, $num_correct)
+    {
+        $difference = abs($num_correct - $guess);
+
+        if ($difference < 10)
+        {
+            $response = "Great guess!";
+            return $response;
+        } elseif ($difference >= 10 and $difference < 50 ){
+            $response = "Very close!";
+            return $response;
+        }
+
+        $response = "You'll get it next time!";
+        return $response;
+    }
+
+    /**
+    * @param $input
+    * @return integer
+    *
+    * Method initiates helper methods setDoor and match in order to calculate the number of
+    * prize doors.
+    */
+
+    private function process($type, $repetitions)
+    {
+        $matching = 0;          // reset number of prize doors
+
+        # Repeat door selection and confirm prize door
+        for ($i=0; $i < $repetitions; $i++) {
+            $choice = rand(1, 3);       // assign door 1 value at random
+            $other1 = $this->setDoor($choice);  // assign door 2 value at random
+            $other2 = $this->TOTAL - ($choice + $other1);   // assign door 3 value
+            $correct = rand(1, 3);  // set the prize door value at random
+
+            # confirm if user choice to stay or change returns prize door.
+            $matching += $this->match($correct, $other1, $other2, $type);
+        }
+
+        # return number of prize doors
+        return $matching;
+    }
+
+    /**
+    * @param $choice
+    * @return integer
+    *
+    * Method assigns number to door 2, ensuring different from door 1
+    */
+    private function setDoor($choice)
+    {
+        $other1 = 0;
+
+        # repeat assignment until door 2 different from door 1
+        while ($other1 == 0)
+        {
+            $other1 = rand(1, 3);
+            if($other1 == $choice)
+            {
+                $other1 = 0;
+            }
+        }
+        return $other1;
+    }
+
+    /**
+    * @param $correct
+    * @param $other1
+    * @param $other2
+    * @return integer
+    *
+    * Method confirms if type selection, either to stay or change, returns prize door or not. If
+    * prize door, return integer value, otherwise return zero.
+    */
+
+    private function match($correct, $other1, $other2, $type)
+    {
+        if ($type == 'change')
+        {
+            if ($other1 == $correct || $other2 == $correct)
+            {
+                return 1;
+            } else {
+                return 0;
+            }
+        } else {
+            if ($other1 == $correct || $other2 == $correct)
+            {
+                return 0;
+            }
+        }
+        return 1;
     }
 
 }
---
+++ resources/views/game/index.blade.php
@@ -0,0 +1,107 @@
+@extends('layouts.master')
+
+@section('content')
+
+    <div class='container'>
+
+        <h1>Monty Hall Game Show</h1>
+        <table>
+            <thead>
+            <tr>
+                <td><img src='images/door.png' alt='Door 1'></td>
+                <td><img src='images/door.png' alt='Door 2'></td>
+                <td><img src='images/door.png' alt='Door 3'></td>
+            </tr>
+
+            </thead>
+            <tbody>
+            <tr>
+                <th>Door 1</th>
+                <th>Door 2</th>
+                <th>Door 3</th>
+            </tr>
+            </tbody>
+        </table>
+
+        <p>
+            Imagine you're at a game show.
+            You are given the choice of three doors, one of which has a prize behind it.
+            You have already chosen Door 1, but there's a twist.
+            The game show host tells you Door 3 does not have a prize behind it and gives you the option to change your choice.
+            Do you have a better chance of choosing the prize door by changing your choice of doors, or staying?
+            What if you are given the same choice many times, with the prize switching doors at random each time?
+            How many times do you think you'd choose the prize door making the same choice?
+        </p>
+
+        <form method='GET' action='/processInput'>
+            <fieldset class='radios'>
+                <div class='row'>
+                    <div class='col-15'>
+                        <label>Do you Change or do you Stay?</label>
+                    </div>
+                    <div class='col-15'>
+                        <label>
+                            <input type='radio'
+                                   name='type'
+                                   value='change' <?php if (isset($type) and $type == 'change') echo 'checked' ?>> Change
+                        </label>
+                    </div>
+                </div>
+                <div class='row'>
+                    <div class='col-15'></div>
+                    <div class='col-15'>
+                        <label>
+                            <input type='radio'
+                                   name='type'
+                                   value='stay' <?php if (isset($type) and $type == 'stay') echo 'checked' ?>> Stay
+                        </label>
+                    </div>
+                </div>
+            </fieldset>
+
+            <div class='row'>
+                <div class='col-15'>
+                    <label>Choose how many times to repeat this same choice:</label>
+                </div>
+                <div class='col-15'>
+                    <select name='repetitions' id='repetitions'>
+                        <option value='choose'>Choose one...</option>
+                        <option value='50'<?php if (isset($repetitions) and $repetitions == '50') echo 'selected' ?>>50</option>
+                        <option value='100' <?php if (isset($repetitions) and $repetitions == '100') echo 'selected'; ?>>100</option>
+                        <option value='500' <?php if (isset($repetitions) and $repetitions == '500') echo 'selected' ?>>500</option>
+                        <option value='1000' <?php if (isset($repetitions) and $repetitions == '1000') echo 'selected' ?>>1000</option>
+                    </select>
+                </div>
+            </div>
+
+            <div class='row'>
+                <div class='col-15'>
+                    <label>Guess how many times the same choice would be right, any number 1 - 999:</label>
+                </div>
+                <div class='col-15'>
+                    <input type='text' name='guess' size='4' value='<?= $guess ?? ' ' ?>'>
+                </div>
+            </div>
+
+            <input type='submit' class='submit' value='Get Answer'>
+        </form>
+
+        <?php if ($num_correct != null): ?>
+        <div class='alert alert-success' role='alert'>
+            By making the choice to <strong><?= $type ?></strong> you would have chosen the prize door
+            <strong><?= $num_correct ?></strong> times out of <strong><?= $repetitions ?></strong> chances, or
+            <strong><?= $percentage ?>%</strong>.
+        </div>
+        <div class='alert alert-primary' role='alert'>
+            You guessed <strong><?= $guess ?></strong>. <?= $response ?>
+        </div>
+        <div class='alert alert-warning' role='alert'>
+            Try changing your choices and see what results you get! To learn more about the Monty Hall
+            problem click <a target='_blank' href='https://en.wikipedia.org/wiki/Monty_Hall_problem'>here</a>.
+        </div>
+        <?php endif ?>
+
+    </div>
+
+
+@endsection
