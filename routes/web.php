49ed41edeb6f7467685c4bbce6f604fd1df5146b
Rupert <rupert.dudeney@gmail.com>
2018-11-07 23:09:16 -0500

    Minor change to Routes page

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@
 Route::get('/', 'GameController@index');
 Route::get('/processInput', 'GameController@processInput');
 
+
+# Routes the user to the static winner page
 Route::view('/winner', 'winner');
 
 
